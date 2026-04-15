bd71182059f50184c384fdb487afa214d1fa5568
AbdallahIbrahim27 <abdallahibrahiim27@gmail.com>
2026-04-15 20:12:12 +0200

    edit name of project

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("💱 Currency Data Pipeline Dashboard")
+st.title("💱 Real-Time Currency Intelligence System")
 st.markdown("Live exchange rates with Refresh feature ⟳")
 
 # -----------------------------
